2a7a80fb73157f4d3f378d0e8f806681a0e192d2
MAL1S <karbushev_ivan@mail.ru>
2023-02-19 16:46:51 +0800

    Added navigation

--- app/src/main/java/com/example/slang_dict/data/local/SlangWord.kt
+++ app/src/main/java/com/example/slang_dict/data/local/SlangWord.kt
@@ -1,11 +1,14 @@
 package com.example.slang_dict.data.local
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity
 data class SlangWord(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val word: String,
     val translates: String
-)
+): Parcelable
